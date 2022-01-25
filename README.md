47f14d1a715a8e81f5ada00d7e4c2f7781bda028
Divmaan Yadav <yadavdivmaan@gmail.com>
2022-01-25 20:49:59 +0530

    push new

---
+++ README.md
@@ -0,0 +1,1 @@
+# B_66_Database
---
+++ divmaan_dbs.js
@@ -0,0 +1,44 @@
+const mongoose = require("mongoose");
+
+//** connection to database */
+const conn_str =
+"mongodb+srv://root:root@cluster0.qejpb.mongodb.net/ddb?retryWrites=true&w=majority";
+
+mongoose
+  .connect(conn_str, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("Connected successfully..."))
+  .catch((error) => console.log(error));
+
+const userSchema = new mongoose.Schema({
+  name: String,
+  age: Number,
+  city: String,
+});
+
+const user = new mongoose.model("user", userSchema);
+
+/** Express Mongoose Integration **/
+const express = require("express");
+const app = express();
+const router = express.Router();
+
+router.get("/:id", async (req, res) => {
+  /** getting user email */
+  console.log(req.params.id);
+  let data = await user.find({ _id: req.params.id });
+  console.log(data);
+
+  // res.send(req.params);
+  res.send(data);
+});
+
+router
+  .route("/")
+  .get(async (req, res) => {
+    let data = await user.find(); // collection_name.find()
+    console.log(data);
+    res.send(data);
+  })
+  
+  module.exports = router;
+  
