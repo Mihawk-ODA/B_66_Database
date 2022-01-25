489092dbccbc6eab699761fa7efa91908be3d360
Divmaan Yadav <yadavdivmaan@gmail.com>
2022-01-25 21:53:50 +0530

    datachecking

--- divmaan_dbs.js
+++ divmaan_dbs.js
@@ -1,8 +1,52 @@
-const mongoose = require("mongoose");
+/*const mongoose = require("mongoose");
+
+ connection to database 
+const conn_str =
+"mongodb+srv://root:root@cluster0.qejpb.mongodb.net/ddb?retryWrites=true&w=majority";
+
+mongoose
+  .connect(conn_str, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("Connected successfully..."))
+  .catch((error) => console.log(error));
+
+const userSchema = new mongoose.Schema({
+  name: String,
+  age: Number,
+  city: String,
+});
+
+const user = new mongoose.model("users", userSchema);
+
+/** Express Mongoose Integration 
+const express = require("express");
+const app = express();
+const router = express.Router();
+
+router.get("/:id", async (req, res) => {
+  /** getting user email 
+  console.log(req.params.id);
+  let data = await user.find({ _id: req.params.id });
+  console.log(data);
+
+  // res.send(req.params);
+  res.send(data);
+});
+
+router
+  .route("/")
+  .get(async (req, res) => {
+    let data = await user.find(); // collection_name.find()
+    console.log(data);
+    res.send(data);
+  })
+  
+  module.exports = router;*/
+
+  const mongoose = require("mongoose");
 
 //** connection to database */
 const conn_str =
-"mongodb+srv://root:root@cluster0.qejpb.mongodb.net/ddb?retryWrites=true&w=majority";
+  "mongodb+srv://root:root@cluster0.8i0tm.mongodb.net/tcet?retryWrites=true&w=majority";
 
 mongoose
   .connect(conn_str, { useNewUrlParser: true, useUnifiedTopology: true })
@@ -39,6 +83,36 @@ router
     console.log(data);
     res.send(data);
   })
-  
-  module.exports = router;
+  .post(async (req, res) => {
+    req_data = req.body;
+    // console.log(req_data);
+    let obj = new user(req_data);
+    let result = await obj.save();
+    console.log(result);
+    // res.send(req_data);
+    res.send(result);
+  })
+  .put(async (req, res) => {
+    req_data = req.body;
+    console.log(req_data.id);
+    let result = await user.updateOne(
+      { _id: req_data.id },
+      {
+        $set: {
+          name: req_data.name,
+          age: req_data.age,
+          city: req_data.city,
+        },
+      }
+    );
+    res.send(result);
+    // res.send(req_data);
+  })
+  .delete(async (req, res) => {
+    let result = await user.deleteOne({ _id: req.query.id });
+    res.send(result);
+  });
+
+module.exports = router;
+
   
